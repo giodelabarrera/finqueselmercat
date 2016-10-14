7cdbc1b0f4bde99a49f338f308c16676aacb6a76
Giorgio de la Barrera <giodelabarrera@gmail.com>
2016-10-14 16:58:07 +0200

    html adminlte

---
+++ src/AppBundle/Form/AddressType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('cp')
+            ->add('cityId')
+            ->add('streetTypeId')
+            ->add('street')
+            ->add('number')
+            ->add('floorId')
+            ->add('stair')
+            ->add('door')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Address'
+        ));
+    }
+}
