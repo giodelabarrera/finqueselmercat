6af4c37ed193a9b22f7181c454a0c2a98f31d01a
Giorgio de la Barrera <giodelabarrera@gmail.com>
2016-10-30 16:16:30 +0100

    preparacion adminlte2

---
+++ src/AppBundle/Controller/Admin/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: giorgio
+ * Date: 30/10/16
+ * Time: 14:48
+ */
+
+namespace AppBundle\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class DashboardController
+ * @package AppBundle\Controller\Admin
+ *
+ * @Route("/admin")
+ */
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/", name="admin_dashboard")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        //$em = $this->getDoctrine()->getManager();
+
+        return $this->render('admin/dashboard/index.html.twig', array(
+            //'bankAwardeds' => $bankAwardeds,
+        ));
+    }
+}
